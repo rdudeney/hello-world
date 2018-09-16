2046ef01bf4ffe062d35e707761d9300d0d811f0
Rupert <rupert.dudeney@gmail.com>
2018-09-16 09:25:48 -0400

    Added updated homepage

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Hello World</title>
+    <meta charset='utf-8'>
+    
+    <link href="css/main.css" rel="stylesheet">
+    
+</head>
+<body>
+    <h1>Hello World</h1>
+    
+    <img src="images/fireworks.jpg">
+    
+</body>
+
+</html>
